92e1141ecac4530601f333bcf0f870e0eadf2531
Ruairi-Munnelly <40793154+Ruairi-Munnelly@users.noreply.github.com>
2024-02-28 15:48:14 +0000

    Updates to audio controls
    Quick update to audio controls to cover autoplay and make looping selectable state.

--- client/src/data.js
+++ client/src/data.js
@@ -1,73 +1,53 @@
-import { v4 as uuidv4 } from "uuid"; 
-function chillHop() { 
-  return [ 
-    { 
-      name: "Sunrise Serenade", 
-      cover: 
-"https://media.geeksforgeeks.org/wp-content/uploads/20210224040124/JSBinCollaborativeJavaScriptDebugging6.png", 
-      artist: " Harmony Harp", 
-      audio: 
-"http://localhost:3000/public/audio/cloudkicker_to_scale_not_painted.mp3", 
-      color: ["#205950", "#2ab3bf"], 
-      id: uuidv4(), 
-      active: true, 
-    }, 
-    { 
-      name: "Urban Groove", 
-      cover: 
-        "https://media.geeksforgeeks.org/wp-content/uploads/20231004210806/DemotivationalPosterfull936506.jpg", 
-      artist: "Beatmaster B", 
-      audio: 
-        "https://media.geeksforgeeks.org/wp-content/uploads/20231004184006/SoundHelix-Song-10.mp3", 
-      color: ["#EF8EA9", "#ab417f"], 
-      id: uuidv4(), 
-      active: false, 
-    }, 
-    { 
-      name: "Mystic Echo", 
-      cover: 
-"https://media.geeksforgeeks.org/wp-content/uploads/20231004210619/3408428b23c516b1687c748cb7de7be7.webp", 
-      artist: " Harmony Harp", 
-      audio: 
-"https://media.geeksforgeeks.org/wp-content/uploads/20231004185212/Jawan-Prevue-Theme.mp3", 
-      color: ["#CD607D", "#c94043"], 
-      id: uuidv4(), 
-      active: false, 
-    }, 
-    { 
-      name: "Electro Vibes", 
-      cover: 
-"https://media.geeksforgeeks.org/wp-content/uploads/20231004184219/gfglogo0.jpg", 
-      artist: "Synthwave Sensation", 
-      audio: 
-"https://media.geeksforgeeks.org/wp-content/uploads/20231004191840/Zinda-Banda---Jawan-(1).mp3", 
-      color: ["#EF8EA9", "#ab417f"], 
-      id: uuidv4(), 
-      active: false, 
-    }, 
-    { 
-      name: "Jazzy Whispers", 
-      cover: 
-"https://media.geeksforgeeks.org/wp-content/uploads/20231004210806/DemotivationalPosterfull936506.jpg", 
-      artist: "Smooth Sax Serenade", 
-      audio: 
-"https://media.geeksforgeeks.org/wp-content/uploads/20231004184006/SoundHelix-Song-10.mp3", 
-      color: ["#CD607D", "#c94043"], 
-      id: uuidv4(), 
-      active: false, 
-    }, 
-    { 
-      name: "Tropical Breez", 
-      cover: 
-"https://media.geeksforgeeks.org/wp-content/uploads/20231004210619/3408428b23c516b1687c748cb7de7be7.webp", 
-      artist: "Island Rhythms", 
-      audio: 
-"https://media.geeksforgeeks.org/wp-content/uploads/20231004191840/Zinda-Banda---Jawan-(1).mp3", 
-      color: ["#205950", "#2ab3bf"], 
-      id: uuidv4(), 
-      active: false, 
-    }, 
-  ]; 
-} 
-  
-export default chillHop;
+import { v4 as uuidv4 } from "uuid";
+import cloudkicker_audio from "./audio/cloudkicker_to_scale_not_painted.mp3";
+import cloudkicker_cover from "./covers/to_scale_not_painted.jpg";
+import mutiny_on_the_bounty_audio from "./audio/Mutiny On The Bounty - Mutiny on the Bounty - Digital Tropics - 10 Sonars.mp3";
+import mutiny_on_the_bounty_cover from "./covers/digital_tropics.jpeg";
+import toe_audio from './audio/toe - For Long Tomorrow - 10 グッドバイ (Album Version).mp3';
+import toe_cover from './covers/for_long_tomorrow.jpeg';
+import russian_circles_audio from './audio/Russian Circles - Memorial - 06 Ethel.mp3';
+import russian_circles_cover from './covers/memorial.jpeg';
+
+function Audio() {
+  return [
+    {
+      name: "To Scale Not Painted",
+      cover: cloudkicker_cover,
+      artist: "Cloudkicker",
+      audio: cloudkicker_audio,
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: "Sonars",
+      cover: mutiny_on_the_bounty_cover,
+      artist: "Mutiny on the Bounty",
+      audio: mutiny_on_the_bounty_audio,
+      color: ["#EF8EA9", "#ab417f"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "グッドバイ",
+      cover: toe_cover,
+      artist: "Toe",
+      audio: toe_audio,
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Ethel",
+      cover:
+        russian_circles_cover,
+      artist: "Russian Circles",
+      audio: russian_circles_audio,
+      color: ["#EF8EA9", "#ab417f"],
+      id: uuidv4(),
+      active: false,
+    },
+  ];
+}
+
+export default Audio;
